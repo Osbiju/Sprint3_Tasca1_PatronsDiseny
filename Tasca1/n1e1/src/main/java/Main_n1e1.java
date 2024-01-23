d38acc3896be6456d3804f8eca00b25b4bab4b42
Oscar <okeshik@protonmail.com>
2024-01-23 13:16:34 +0100

    Ready per corregir

--- Tasca1/n1e1/src/main/java/Main_n1e1.java
+++ Tasca1/n1e1/src/main/java/Main_n1e1.java
@@ -1,7 +1,45 @@
+import java.util.Scanner;
+
 public class Main_n1e1 {
     public static void main(String[] args) {
 
-        Undo_Singleton undo = Undo_Singleton.getInstance();
+        Scanner entrada = new Scanner(System.in);
+        boolean menu = true;
+        int opcio = 0;
 
+        Undo_Singleton undo = Undo_Singleton.getInstancia();
+
+        while (menu){
+            System.out.println("Escull una de les seguents opcions: \n" +
+                    "1. Afegir comanda \n" +
+                    "2. Esborrar ultima comanda\n" +
+                    "3. Llistar historial\n" +
+                    "4. Sortir");
+
+            opcio = entrada.nextInt();
+            entrada.nextLine();
+
+            switch (opcio){
+                case 1:
+                    System.out.println("Introdueix la comanda a afegir:");
+                    String comanda = entrada.nextLine();
+                    undo.afegirComanda(comanda);
+                    System.out.println("Correcte, has afegit " + comanda + "\n" );
+                    break;
+                case 2:
+                    undo.eliminarComanda();
+                    break;
+                case 3:
+                    undo.llistarComanda();
+                    break;
+                case 4:
+                    System.out.println("Has sortit del programa");
+                    menu = false;
+                default:
+                    System.out.println("L'opcio seleccionada no es correcta");
+            }
+        }
+        entrada.close();
     }
+
 }
--- Tasca1/n1e1/src/main/java/Undo_Singleton.java
+++ Tasca1/n1e1/src/main/java/Undo_Singleton.java
@@ -4,28 +4,49 @@ import java.util.List;
 public class Undo_Singleton {
     //Decalracio
     private static Undo_Singleton instancia;
-    private List<String> comandes;
+    private List<String> comandes = new ArrayList<String>();
 
     private Undo_Singleton() {
+        //comandes = new ArrayList<>(); //es podria instanciar aqui tmb en comptes de com atribut
+
         //constructor private i buit, per evitar instancia amb "new"
     }
 
 
     //getter de instance amb la instancia del objecte undo si es null
-    public static Undo_Singleton getInstance() {
+    public static Undo_Singleton getInstancia() {
         if (instancia == null) {
             instancia = new Undo_Singleton();
         }
         return instancia;
     }
 
-    //metode per guardar comandes
 
     //metode per afegir comandes
+    public void afegirComanda(String comanda){
+        comandes.add(comanda);
+    }
 
     //metode per eliminar comandes
+    public void eliminarComanda(){
+        if(!comandes.isEmpty()){
+            comandes.remove(comandes.size() - 1);
+        } else {
+            System.out.println("No tenim comandes a eliminar");
+        }
+    }
 
     //metode per llistar comandes
+    public void llistarComanda(){
+        if(!comandes.isEmpty()){
+            System.out.println("Llistat de comandes:");
+            for (String comanda : comandes){
+                System.out.println(comanda);
+            }
+        } else {
+            System.out.println("No hi ha comandes per llistar");
+        }
+    }
 
 
 }
